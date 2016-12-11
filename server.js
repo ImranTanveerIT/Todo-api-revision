f44819b07c38ac01d0e73e73545639b8d71d5126
Imran Tanveer <imarn.tanveer@seecs.edu.pk>
2016-12-11 21:37:37 +0500

    refactored using _ lib

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 
 var app = express();
-
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
 
 var todos = [{
 	id : 1,
@@ -27,18 +34,21 @@ app.get('/todos',function(req,res){
 	res.json(todos);
 });
 
+//Get /todos/:id
 app.get('/todos/:id',function(req,res){
 
 	var todoId = parseInt(req.params.id,10);
-	var matchedTodo;
-	//res.send('Asking for todo with id '+todoId);
 
+	//with underscore lib
+	var matchedTodo = _.findWhere(todos,{id:todoId});
+
+	//without underscore lib
+	/*var matchedTodo;
 	todos.forEach(function(todo){
-		//res.send(' id '+todo.id);
 		if(todoId === todo.id){
 			matchedTodo = todo;
 		}
-	});
+	});*/
 
 	if(matchedTodo){
 		res.json(matchedTodo);
@@ -48,8 +58,50 @@ app.get('/todos/:id',function(req,res){
 	
 });
 
-//Get /todos/:id
+
+//Post  /todo
+
+app.post('/todos',function(req,res){
+
+	var body = req.body;
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	//remove the unwanted data
+	body = _.pick(body,'description','completed');
+
+	//trim the space from description
+	body.description = body.description.trim();
+	/*var todo = {
+		id : todoNextId,
+		description : body.description,
+		completed : body.completed
+	};*/
+
+
+
+	body.id = todoNextId++;
+	todos.push(body);
+
+	//todoNextId++;
+
+	console.log('description '+body.description);
+
+	res.json(body);
+});
 
 app.listen(PORT,function(){
 	console.log('express listening on port '+PORT);
 });
+
+
+
+
+
+
+
+
+
+
