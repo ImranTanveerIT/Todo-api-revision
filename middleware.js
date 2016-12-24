00c3a57d9ef1a5d9e772d94b17069444ec422970
Imran Tanveer <imarn.tanveer@seecs.edu.pk>
2016-12-24 22:04:46 +0500

    added log out

--- middleware.js
+++ middleware.js
@@ -1,18 +1,46 @@
 // this file contains middleware and it runs before the regular route handler
 
+var cryptojs = require('crypto-js');
 module.exports = function(db) {
 
 	return {
 		requireAuthentication : function(req,res,next){
 			var token = req.get('Auth');
 
-			db.user.findByToken(token).then(function(user){
+
+			db.token.findOne({
+				where : {
+					tokenHash : cryptojs.MD5(token).toString()
+				}
+			}).then(function(tokenInstance){
+
+				if(!tokenInstance){
+					throw new Error();
+				}
+
+				req.token = tokenInstance;
+				return db.user.findByToken(token);
+			}).then(function(user){
+				req.user = user;
+				next();
+			}).catch(function(){
+				res.status(401).send();
+			});
+
+
+
+
+
+
+
+			// this code was used before lecture 85 but now the token will be saved in db after being hashed
+			/*db.user.findByToken(token).then(function(user){
 				req.user = user;
 				next();
 			},function(){
 				res.status(401).send();
-			});
+			});*/
 		}
-	}
+	};
 
 };
--- server.js
+++ server.js
@@ -384,11 +384,23 @@ app.post('/users',function(req,res){
 
 })
 
+// DELETE /users/login
+app.delete('/users/login',middleware.requireAuthentication,function(req,res){
+
+	req.token.destroy().then(function(){
+		res.status(204).send();
+	}).catch(function(){
+		res.status(500).send();
+	});
+});
+
+
 
 //lecture 76
 app.post('/users/login',function(req,res){
 
 	var body = _.pick(req.body,'email','password');
+	var userInstance;
 
 	/*
 		authenticate is our own made method and the code inside it was initially here and then moved in
@@ -396,13 +408,24 @@ app.post('/users/login',function(req,res){
 	*/
 	db.user.authenticate(body).then(function(user){ 
 		var token = user.generateToken('authentication');
-		if(token){
+		userInstance = user;
+
+		//saving token in db
+		return db.token.create({
+			token:token
+		});
+
+		//this is commented in lecture 84 bcz now the token will be saved in db in hashed form which will be used in
+		// logging out user
+		/*if(token){
 			res.header('Auth',token  ).json(user.toPublicJSON());	
 		}else{
 			res.status(401).send();
-		}
+		}*/
 		
-	},function(e){
+	}).then(function(tokenIntance){
+		res.header('Auth',tokenIntance.get('token')).json(userInstance.toPublicJSON());	
+	}).catch(function(){
 		res.status(401).send();
 	});
 });
